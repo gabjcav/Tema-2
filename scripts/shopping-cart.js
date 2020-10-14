696c89aa22102e07cbed5a84c86fafbca5f770ef
gabjcav <69957148+gabjcav@users.noreply.github.com>
2020-10-14 10:54:30 +0200

    made all files i need and made a footer

---
+++ scripts/shopping-cart.js
@@ -0,0 +1,13 @@
+const MAIN_CONTAINER = document.querySelector(".main-container");
+
+let shoppingCart = [];
+
+if (shoppingCart[i] === ""){
+    MAIN_CONTAINER.innerHTML += `
+        <div class = "empty-cart"> <h1>There is nothing in your shopping cart.</h1> </div>
+    `
+};
+
+else{
+    console.log("Hei");
+}
