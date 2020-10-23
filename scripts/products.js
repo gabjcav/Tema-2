d6648c9e064b3339f77da1a1bbd01493f9e5682a
gabjcav <69957148+gabjcav@users.noreply.github.com>
2020-10-23 09:14:43 +0200

    dot slash all img

--- scripts/products.js
+++ scripts/products.js
@@ -25,7 +25,7 @@ export let gamesGrid = [
     {
       name: "GRAND THEFT AUTO V (PS4)",
       price: 35,
-      url: '/img/cover/ps4/NEWS/gta.jpeg',
+      url: './img/cover/ps4/NEWS/gta.jpeg',
       category: "popular",
       id: 3,
       qty: 0,
@@ -35,7 +35,7 @@ export let gamesGrid = [
   
       name: "SEA OF THIEVES (XBOX)",
       price: 29,
-      url: '/img/cover/xbox/SOT.jpg',
+      url: './img/cover/xbox/SOT.jpg',
       category: "popular",
       id: 4,
       qty: 0,
@@ -47,7 +47,7 @@ export let gamesGrid = [
     {
       name: "SQUAD (PC)",
       price: 39,
-      url: '/img/cover/pc/squad.jpg',
+      url: './img/cover/pc/squad.jpg',
       category: "pc",
       id: 5,
       qty: 0,
@@ -56,7 +56,7 @@ export let gamesGrid = [
     {
       name: "GRAND THEFT AUTO V (PC)",
       price: 35,
-      url: '/img/cover/pc/gta.jpg',
+      url: './img/cover/pc/gta.jpg',
       category: "pc",
       id: 6,
       qty: 0,
@@ -65,7 +65,7 @@ export let gamesGrid = [
     {
       name: "CALL OF DUTY: COLD WAR (PC)",
       price: 69,
-      url: '/img/cover/pc/codcw.jpeg',
+      url: './img/cover/pc/codcw.jpeg',
       category: "pc",
       id: 7,
       qty: 0,
@@ -75,7 +75,7 @@ export let gamesGrid = [
   
       name: "BATTLEFIELD V (PC)",
       price: 45,
-      url: '/img/cover/pc/bf5.jpg',
+      url: './img/cover/pc/bf5.jpg',
       category: "pc",
       id: 8,
       qty: 0,
@@ -88,7 +88,7 @@ export let gamesGrid = [
     {
       name: "FIFA 21 (PS4)",
       price: 59,
-      url: '/img/cover/ps4/NEWS/193479.png',
+      url: './img/cover/ps4/NEWS/193479.png',
       category: "ps4",
       id: 9,
       qty: 0,
@@ -97,7 +97,7 @@ export let gamesGrid = [
     {
       name: "FARCRY 5 - GOLD EDITION (PS4)",
       price: 39,
-      url: '/img/cover/ps4/NEWS/farcry.jpg',
+      url: './img/cover/ps4/NEWS/farcry.jpg',
       category: "ps4",
       id: 10,
       qty: 0,
@@ -106,7 +106,7 @@ export let gamesGrid = [
     {
       name: "GRAND THEFT AUTO V (PS4)",
       price: 35,
-      url: '/img/cover/ps4/NEWS/gta.jpeg',
+      url: './img/cover/ps4/NEWS/gta.jpeg',
       category: "ps4",
       id: 11,
       qty: 0,
@@ -116,7 +116,7 @@ export let gamesGrid = [
   
       name: "CALL OF DUTY: MODERN WARFARE (PS4)",
       price: 49,
-      url: '/img/cover/ps4/NEWS/mw.jpg',
+      url: './img/cover/ps4/NEWS/mw.jpg',
       category: "ps4",
       id: 12,
       qty: 0,
@@ -129,7 +129,7 @@ export let gamesGrid = [
     {
       name: "JUST CAUSE 4 (XBOX)",
       price: 59,
-      url: '/img/cover/xbox/JC4.jpg',
+      url: './img/cover/xbox/JC4.jpg',
       category: "xbox",
       id: 13,
       qty: 0,
@@ -138,7 +138,7 @@ export let gamesGrid = [
     {
       name: "SEA OF THIEVES (XBOX)",
       price: 29,
-      url: '/img/cover/xbox/SOT.jpg',
+      url: './img/cover/xbox/SOT.jpg',
       category: "xbox",
       id: 14,
       qty: 0,
@@ -147,7 +147,7 @@ export let gamesGrid = [
     {
       name: "FARCRY 5 - DELUXE EDITION (XBOX)",
       price: 55,
-      url: '/img/cover/xbox/FARCRY5DELUXE.jpg',
+      url: './img/cover/xbox/FARCRY5DELUXE.jpg',
       category: "xbox",
       id: 15,
       qty: 0,
@@ -157,7 +157,7 @@ export let gamesGrid = [
   
       name: "FIFA 21 - CHAMPIONS EDITION (XBOX)",
       price: 79,
-      url: '/img/cover/xbox/fifa21.png_large',
+      url: './img/cover/xbox/fifa21.png_large',
       category: "xbox",
       id: 16,
       qty: 0,
